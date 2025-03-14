5fbe357180036024879a16c136f33c24cad21034
Chaitanya-5g3 <uchaitanyaraji@gmail.com>
2025-03-14 16:55:13 +0530

    Create Seasons.py

---
+++ Seasons.py
@@ -0,0 +1,9 @@
+month=int(input())
+if 3<=month<=6:
+   print("summer")
+elif 7<=month<=10:
+   print("rainy")
+elif month<=12 or month<=2:
+   print("winter")
+elif 12<month:
+   print("Invalid month")
